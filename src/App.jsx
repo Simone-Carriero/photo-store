fafaab16e54acc9613dd190dd6030ccf6830aecf
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-09 11:07:17 +0100

    Add basic routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Cart, Photos, Error, Favorite, SharedLayout } from './pages';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<SharedLayout />}>
+          <Route
+            index
+            element={<Photos />}
+          />
+          <Route
+            path='favorite'
+            element={<Favorite />}
+          />
+          <Route
+            path='cart'
+            element={<Cart />}
+          />
+        </Route>
+        <Route
+          path='*'
+          element={<Error />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import SharedLayout from './SharedLayout';
+import Photos from './Photos';
+import Cart from './Cart';
+import Favorite from './Favorite';
+import Error from './Error';
+
+export { SharedLayout, Photos, Cart, Favorite, Error };
