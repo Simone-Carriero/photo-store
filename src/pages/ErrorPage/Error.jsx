8e97e636e8b7b98f729a0f2438937f403ae94615
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-09 13:39:24 +0100

    Add Error page

---
+++ src/pages/ErrorPage/Error.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Error.css';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <section className='error'>
+      <div className='error__container'>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link
+          to='/'
+          className='btn'>
+          Back Home
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Error;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import SharedLayout from './SharedLayout';
 import Photos from './Photos';
 import Cart from './Cart';
 import Favorite from './Favorite';
-import Error from './Error';
+import Error from './ErrorPage/Error';
 
 export { SharedLayout, Photos, Cart, Favorite, Error };
