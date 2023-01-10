a56809ee8a44791aee6fedba8c9e4799cc1dd534
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-10 10:57:02 +0100

    Add useDocumentTitle hook and use it on the pages

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import useDocumentTitle from '../hooks/useDocumentTitle';
 
 const Cart = () => {
+  useDocumentTitle('Cart');
   return <div>Cart</div>;
 };
 
--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PhotosContainer from '../components/PhotosContainer/PhotosContainer';
 import { useGlobalContext } from '../context/GlobalContext';
+import useDocumentTitle from '../hooks/useDocumentTitle';
 
 const Photos = () => {
+  useDocumentTitle('Homepage');
   const { allPhotos } = useGlobalContext();
   return (
     <section>
