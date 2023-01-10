05bd24cf496cf714f37f8e1ce5f004088c626efd
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-10 11:16:58 +0100

    Complete Favorite page

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PhotosContainer from '../components/PhotosContainer/PhotosContainer';
+import { useGlobalContext } from '../context/GlobalContext';
 import useDocumentTitle from '../hooks/useDocumentTitle';
 
 const Favorite = () => {
   useDocumentTitle('Favorite');
-  return <div>Favorite</div>;
+  const { allPhotos } = useGlobalContext();
+  const favoritePhotos = allPhotos.filter(photo => photo.isFavorite)
+  return (
+    <section>
+      <PhotosContainer allPhotos={favoritePhotos} />
+    </section>
+  );
 };
 
 export default Favorite;
